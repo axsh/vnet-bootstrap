20d39413cfd9b5410fc990dbb6f0e05205aa6b7f
Richard Potter <potter@axsh.net>
2014-04-14 00:26:22 +0900

    Prevent multiple start-all.sh processes on one VM and unwanted VM regeneration

--- lib/vnet-install-script/start-all.sh
+++ lib/vnet-install-script/start-all.sh
@@ -18,6 +18,12 @@ divider()
     echo
 }
 
+verfify-not-stopped()
+{
+    # if ./bin/stop-all-vms.sh removes a VM, don't restart it automatically
+    [ -d vm$vmid ] || reportfail "Stopping because vm$vmid apparently forcibly removed."
+}
+
 do-until-done()
 {
     vmid="$1"
@@ -27,6 +33,15 @@ do-until-done()
     echo $ccc >inprogress-$vmid
     while [ -f inprogress-$vmid ]
     do
+	if [ -f "inprogress-$vmid.pid" ]
+	then
+	    claimer="$(< "inprogress-$vmid.pid")"
+	    if [ -d "/proc/$claimer" ] # still alive
+	    then
+	        [ "$claimer" == "$$" ] || reportfail "There appears to be another start-all.sh running on vm$vmid"
+	    fi
+	fi
+	echo "$$" >"inprogress-$vmid.pid"
 	snapshot="" # parameter will be removed below during expansion (note: spaces in snapshot name not allowed)
 	snset=""
 	[ -d ../snapshots ] && snset="../snapshots"
@@ -42,17 +57,19 @@ do-until-done()
 	echo "$!" >pid$vmid
 	sleep 2
 	wait
+	verfify-not-stopped
 	result="$(./lib/c-dinkvm/dinkvm -mem "$mem" vm$vmid ... sudo bash  \
 	    onhost/lib/vnet-install-script/test-vnet-in-dinkvm.sh check1 "$@" &
             )"
 	if [[ "$result" != *Not* ]]
 	then
 	    echo "Success after $(cat inprogress-$vmid 2>/dev/null) attempts for vm$vmid." >>log$vmid
-	    rm inprogress-$vmid
+	    rm inprogress-$vmid inprogress-$vmid.pid
 	else
 	    echo "About to try again for vm$vmid...." 1>&2
 	    sleep 10 # to avoid hogging CPU if things go really bad
 	fi
+	verfify-not-stopped
     done
 }
 
