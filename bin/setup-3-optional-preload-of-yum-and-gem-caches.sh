b1d1cadfc3e0573a9b5ef9ad88397e27d420927f
Richard Potter <potter@axsh.net>
2014-11-11 23:49:12 +0900

    Reset URL from kagami to 2.24 for setup-3-optional-preload-of-yum-and-gem-caches.sh

--- bin/setup-3-optional-preload-of-yum-and-gem-caches.sh
+++ bin/setup-3-optional-preload-of-yum-and-gem-caches.sh
@@ -16,5 +16,5 @@ then
     echo "do 'rm -fr var-cache-yum vnet-vendor' and run again for fresh cache preload."
 else
     cd "$SCRIPT_DIR/.."
-    curl http://192.168.2.26:2579/cache-v4.tar.gz | tar xz
+    curl http://192.168.2.24:2579/cache.tar.gz | tar xz
 fi
